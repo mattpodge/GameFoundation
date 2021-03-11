8c782c077f9f9f8fd12ea37fd378f0e394462d8c
Matt <mattpodge@gmail.com>
2021-03-11 18:21:05 +0000

    Quick player implemented with x axis movement

---
+++ Assets/Scripts/Monobehaviour/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Input Settings")]
+    public PlayerInput playerActions;
+
+    private Vector3 rawInputMovement;
+
+    void Update()
+    {
+        transform.Translate(rawInputMovement * 10 * Time.deltaTime);
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 movementInput = value.ReadValue<Vector2>();
+        rawInputMovement = new Vector3(movementInput.x, 0, 0);
+
+    }
+
+}
